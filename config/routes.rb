f2cce7147cd990597b19b48a839cbaacf82eb59a
Cristi Zanfir <zanfircristian92@gmail.com>
2018-01-28 11:33:08 +0000

    Add friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => { :registrations => "user/registrations" }
+  resources :user_stocks, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  resources :user_stocks, only: [:create, :destroy]
+  get 'my_friends', to: "users#my_friends"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
